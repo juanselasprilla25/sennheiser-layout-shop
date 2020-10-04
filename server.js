b0dd649d7e0c08d04f1240422cdad724c25a3078
juanse941025 <juan.2510@hotmail.com>
2020-10-03 23:29:11 -0500

    First Deploy

--- server.js
+++ server.js
@@ -2,11 +2,33 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
-app.listen(port, () => {
-  console.log("Server is up!");
-});
+app.set("port", process.env.PORT || 3000);
+
+/**
+ * Static Files
+ */
+// app.use(express.static(publicPath));
+app.use(express.static("build"));
+
+/**
+ * Default route
+ */
+if (process.env.NODE_ENV == "production") {
+  app.get("/", function (req, res) {
+    res.sendFile(path.resolve("build/index.html"));
+  });
+} else {
+  //   app.get("/", function (req, res) {
+  //     res.send("Listening on port :" + app.get("port"));
+  //   });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"));
+  });
+}
+
+/**
+ * StartingServer
+ */
+app.listen(app.get("port"), () =>
+  console.log("Running server on REACT SCRIPT port " + app.get("port"))
+);
